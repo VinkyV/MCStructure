2b68a6f611866e5a5cc4781b9df0f74baaa45f1d
VinkyV <75137786+VinkyV@users.noreply.github.com>
2024-07-27 20:56:36 +0300

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ export default defineUserConfig({
   lang: 'en-US',
 
   title: 'MC TIL',
-  base: '/MCstructure/',
+  base: '/MCStructure/',
   description: 'Some structure and enum information for minecraft bedrock edition.',
   plugins: [
     activeHeaderLinksPlugin(),
